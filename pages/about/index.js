a1e7d25470c91874a64c87e2ce7f95b6d44cff5f
yudhajit <yudhajitadhikary@gmail.com>
2022-02-11 00:50:57 +0530

    added changes

--- pages/about/index.js
+++ pages/about/index.js
@@ -13,6 +13,9 @@ export default function About() {
         <p className={styles.nameStyle}>Bringing forth expertise in design, installation, testing and maintenance of web systems. </p>
         <p className={styles.nameStyle}>Equipped with a diverse and promising skillset.</p>
         <p className={styles.nameStyle}>Want to connect with me ?&nbsp;<a target="_blank" className={styles.anchor} href="/contact">Let's talk</a>.</p>
+        <p className={styles.nameStyle}>Want to know more on my&nbsp;<a className={styles.anchor} href="/contact">Technical Skill ?</a></p>
+        {/* Meticulous web developer with over 3 years of front end experience and passion for responsive website design and a firm believer in the mobile-first approach. Implemented new responsive website approach with complete unit testing and SEO which increases performance and traffic of the website */}
+
         </div>
         </div>
     )
