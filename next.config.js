157276bdc727a0ce8b1068120e06e09fe3eee3f3
yudhajit <yudhajitadhikary@gmail.com>
2022-03-26 16:19:15 +0530

    added fixs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     basePath:'/Portfolio',
-    assetPrefix:'/Portfolio',
+    assetPrefix:'/Portfolio/',
     webpack: (config, { isServer }) => {
         if (!isServer) {
             // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
