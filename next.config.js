e52014c18d10cd41f8863e37965725aa196d4630
yudhajit <yudhajitadhikary@gmail.com>
2024-05-01 18:49:09 +0530

    Added fixs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     images:{
         disableStaticImages:true,
+        loader:'akamai',
         domains:['img.icons8.com']
     },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
