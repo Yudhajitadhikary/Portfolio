6fbba5ed38ee54d4c4a4ea5fc893a9358fa43da5
yudhajit <yudhajitadhikary@gmail.com>
2024-05-01 18:38:28 +0530

    testing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     images:{
         disableStaticImages:true,
-        loader:'custom',
         domains:['img.icons8.com']
     },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
